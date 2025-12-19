191e0e74fe2e254984c1111b7d9702eb3b3af9ca
NoUsername8 <beat.gruener@web.de>
2025-12-19 16:37:56 +0100

    Removed unused files

--- SchwarzschildMetric.h
+++
@@ -1,7 +0,0 @@
-#ifndef SCHWARZSCHILDMETRIC_H
-#define SCHWARZSCHILDMETRIC_H
-
-double Christoffel(int index1, int index2, int index3);
-TransformingVector GetAcceleration(TransformingVector velocity, double r, double t);
-
-#endif
